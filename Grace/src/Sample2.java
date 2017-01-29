34509b6544db784daac13864eb9320915f4667ca
yuehu <huyue2255@gmail.com>
2017-01-29 15:43:02 -0800

    Add Grace Folder

---
+++ Grace/src/Sample2.java
@@ -0,0 +1,22 @@
+/**
+ * Created by yuehu on 1/29/17.
+ */
+public class Sample2 {
+
+    public static void main(String[] args) {
+
+        // Configuration
+
+        int[] input = {2, 7, 11, 15};
+        int target = 9;
+
+        // Timer for Speed Test
+        long speedX = System.currentTimeMillis();
+
+
+        // Report for Speed Test
+        System.out.println("Time spent :" + (System.currentTimeMillis() - speedX) + "msec");
+
+    }
+
+}
---
+++ Grace/src/Sample4.java
@@ -0,0 +1,6 @@
+
+/**
+ * Created by yuehu on 1/29/17.
+ */
+public class Sample4 {
+}
