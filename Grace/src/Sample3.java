7794ea2a7f43743b39546c23cb1d8d98454d0ed5
yuehu <huyue2255@gmail.com>
2017-01-29 15:50:39 -0800

    update2

---
+++ Grace/src/Sample3.java
@@ -0,0 +1,14 @@
+/**
+ * Created by yuehu on 1/29/17.
+ */
+public class Sample3 {
+    public static void main(String[] args) {
+        // TODO code application logic here
+//        Sample1 s = new Sample1();
+
+//        System.out.println(s.strStr("source", "r"));
+
+        System.out.println("hello Grace2017");
+
+    }
+}
